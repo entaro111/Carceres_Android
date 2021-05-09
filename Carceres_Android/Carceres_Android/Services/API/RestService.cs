32080f936a530baee67acf9e40eb89e91e7c6c6a
entaro111 <krzyjaskowski@gmail.com>
2021-05-09 10:26:16 +0200

    app can connect to localhost api

---
+++ Carceres_Android/Carceres_Android/Services/API/RestService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carceres_Android
+{
+    class RestService
+    {
+        public static string RestUrl = "http://10.0.0.2";
+
+        HttpClient client;      
+        public string odpowiedz { get; private set; }
+
+        public RestService()
+        {
+            client = new HttpClient();
+            
+        }
+
+        public async Task<string> TakeData()
+        {
+            
+            Uri uri = new Uri(string.Format(RestUrl, string.Empty));
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(@"\tERROR {0}", ex.Message);
+            }
+
+            return odpowiedz;
+        }
+    }
+}
--- Carceres_Android/Carceres_Android/ViewModels/LoginViewModel.cs
+++ Carceres_Android/Carceres_Android/ViewModels/LoginViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using Carceres_Android.Views;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace Carceres_Android.ViewModels
@@ -9,16 +12,43 @@ namespace Carceres_Android.ViewModels
     public class LoginViewModel : BaseViewModel
     {
         public Command LoginCommand { get; }
+        string text;
 
         public LoginViewModel()
         {
             LoginCommand = new Command(OnLoginClicked);
+            
         }
 
         private async void OnLoginClicked(object obj)
         {
+            text = await Read();
             // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
             await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+        }
+
+
+        public async Task<string> Read()
+        {
+
+            HttpClient client = new HttpClient();
+            Uri uri = new Uri(string.Format("http://10.0.2.2:43343/api/login", string.Empty));
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(@"\tERROR {0}", ex.Message);
+            }
+
+            return text;
+ 
         }
     }
 }
